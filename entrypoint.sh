bfb0599539086ff25d0efee8f7d38c5615c044d3
hesenismayilov44 <hesenismayilov44@gmail.com>
2022-05-03 05:00:18 +0400

    commiting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn ResApi.wsgi:application --bind 0.0.0.0:8000
+uwsgi --socket :8000 --master --enable-threads --module app.wsgi
 
